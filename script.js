ba92f39d79a653b997f494ff5456afcbee264c3d
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-01-21 22:55:51 -0300

    add script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const imgs = document.getElementById("imgs");
+
+const img = document.querySelectorAll("#imgs img");
+
+let idx = 0;
+
+setInterval(run, 2000);
+
+function run() {
+    idx++;
+
+    if (idx > img.length - 1) {
+        idx = 0;
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)`;
+}
+
